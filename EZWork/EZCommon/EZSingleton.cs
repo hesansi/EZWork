fcce4d614fe8b10d284a76d8e3b123e8f3975990
HeJuncheng <184141795@qq.com>
2020-08-13 11:53:32 +0800

    【EZSingleton】添加非 mono 单例

--- EZWork/EZCommon/EZSingleton.cs
+++ EZWork/EZCommon/EZSingleton.cs
@@ -2,13 +2,35 @@
 
 namespace EZWork
 {
+    public class EZSingleton<T> where T : new()
+    {
+        private static T _instance;
+        private static readonly object objlock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null) {
+                    lock (objlock) {
+                        if (_instance == null) {
+                            _instance = new T();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+    
     /// <summary>
     /// Be aware this will not prevent a non singleton constructor
     ///   such as `T myT = new T();`
     /// To prevent that, add `protected T () {}` to your singleton class.
     /// As a note, this is made as MonoBehaviour because we need Coroutines.
     /// </summary>
-    public class EZSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    public class EZSingletonMono<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T _instance;
         private static object _lock = new object();
@@ -19,64 +41,17 @@ namespace EZWork
             {
                 lock (_lock) {
                     if (_instance != null) return _instance;
-
+                    
+                    // 先在场景中找寻
                     _instance = (T) FindObjectOfType(typeof(T));
-
                     if (FindObjectsOfType(typeof(T)).Length > 1) {
                         Debug.LogError("[Singleton] Something went really wrong " +
                                        " - there should never be more than 1 singleton!" +
                                        " Reopening the scene might fix it.");
                         return _instance;
                     }
-
-                    if (_instance == null) {
-                        GameObject singleton = new GameObject();
-                        _instance = singleton.AddComponent<T>();
-                        singleton.name = "(singleton) " + typeof(T).ToString();
-
-
-
-                        Debug.Log("[Singleton] An instance of " + typeof(T) +
-                                  " is needed in the scene, so '" + singleton +
-                                  "' was created.");
-                    }
-                    else {
-                        Debug.Log("[Singleton] Using instance already created: " +
-                                  _instance.gameObject.name);
-                    }
-
-                    return _instance;
-                }
-            }
-        }
-
-        private void OnDestroy()
-        {
-            _instance = null;
-        }
-    }
-    
-    public class EZSingletonStatic<T> : MonoBehaviour where T : MonoBehaviour
-    {
-        private static T _instance;
-        private static object _lock = new object();
-
-        public static T Instance
-        {
-            get
-            {
-                lock (_lock) {
-                    if (_instance != null) return _instance;
-
-                    _instance = (T) FindObjectOfType(typeof(T));
-
-                    if (FindObjectsOfType(typeof(T)).Length > 1) {
-                        Debug.LogError("[Singleton] Something went really wrong " +
-                                       " - there should never be more than 1 singleton!" +
-                                       " Reopening the scene might fix it.");
-                        return _instance;
-                    }
-
+                    
+                    // 场景中找不到就创建新物体挂载
                     if (_instance == null) {
                         GameObject singleton = new GameObject();
                         _instance = singleton.AddComponent<T>();
@@ -103,4 +78,52 @@ namespace EZWork
         }
     }
     
+    /// 与 EZSingletonMono 实现完全一致，已废弃。为了兼容老版本而存在
+    public class EZSingletonMonoStatic<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        private static object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock) {
+                    if (_instance != null) return _instance;
+                    
+                    // 先在场景中找寻
+                    _instance = (T) FindObjectOfType(typeof(T));
+                    if (FindObjectsOfType(typeof(T)).Length > 1) {
+                        Debug.LogError("[Singleton] Something went really wrong " +
+                                       " - there should never be more than 1 singleton!" +
+                                       " Reopening the scene might fix it.");
+                        return _instance;
+                    }
+                    
+                    // 场景中找不到就创建新物体挂载
+                    if (_instance == null) {
+                        GameObject singleton = new GameObject();
+                        _instance = singleton.AddComponent<T>();
+                        singleton.name = "(singleton) " + typeof(T).ToString();
+
+                        DontDestroyOnLoad(singleton);
+                        Debug.Log("[Singleton] An instance of " + typeof(T) +
+                                  " is needed in the scene, so '" + singleton +
+                                  "' was created.");
+                    }
+                    else {
+                        Debug.Log("[Singleton] Using instance already created: " +
+                                  _instance.gameObject.name);
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _instance = null;
+        }
+    }
 }
